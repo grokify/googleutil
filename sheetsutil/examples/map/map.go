bef7939e8e619f597b4257689ddf38af751ffd29
grokify <johncwang@gmail.com>
2018-04-26 02:21:49 -0700

    update example

--- sheetsutil/examples/map/map.go
+++ sheetsutil/examples/map/map.go
@@ -13,17 +13,17 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
-func GetSheetsMap() (sheetsmap.SheetsMap, error) {
+func GetSheetsMap() (*sheetsmap.SheetsMap, error) {
 	log.SetLevel(log.DebugLevel)
 
 	err := config.LoadDotEnvSkipEmpty(os.Getenv("ENV_PATH"), "./.env")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	jwt := os.Getenv("GOOGLE_SERVICE_ACCOUNT_JWT")
 	if len(jwt) < 1 {
-		log.Fatal("E_NO_JWT")
+		return nil, fmt.Errorf("No Google JWT")
 	}
 	fmt.Println(jwt)
 
@@ -32,13 +32,17 @@ func GetSheetsMap() (sheetsmap.SheetsMap, error) {
 		[]byte(jwt),
 		sheets.DriveScope,
 		sheets.SpreadsheetsScope)
+	if err != nil {
+		return nil, err
+	}
 
-	spreadsheetId := "1ycMSs79QaOQD3oMrHJScTXFN32tNbU3th1Dy4jtT2zc"
+	spreadsheetId := os.Getenv("GOOGLE_SPREADSHEET_ID")
 
-	return sheetsmap.NewSheetsMap(
+	sm, err := sheetsmap.NewSheetsMap(
 		googleClient,
 		spreadsheetId,
 		uint(0))
+	return &sm, err
 }
 
 func main() {
