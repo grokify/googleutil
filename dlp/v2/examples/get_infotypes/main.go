8f4c8cfe230f2f7f7997abf8933685e70045b94e
grokify <johncwang@gmail.com>
2018-07-23 23:32:00 -0700

    update Google DLP Examples

---
+++ dlp/v2/examples/get_infotypes/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/grokify/gotilla/fmt/fmtutil"
+
+	"github.com/jessevdk/go-flags"
+	"google.golang.org/api/option"
+
+	dlp "cloud.google.com/go/dlp/apiv2"
+	dlppb "google.golang.org/genproto/googleapis/privacy/dlp/v2"
+)
+
+/*
+
+https://godoc.org/cloud.google.com/go/dlp/apiv2
+https://cloud.google.com/dlp/docs/auth
+https://dlp.googleapis.com/v2/infoTypes
+
+Note: Granting the user the Owner role is not necessary.
+
+This is based on the following Quickstart
+
+https://github.com/GoogleCloudPlatform/golang-samples/blob/master/dlp/dlp_quickstart/quickstart.go
+
+*/
+
+type Args struct {
+	// Service Account Credentials File
+	CredentialsFile string `short:"c" long:"credentials" description:"Path to crdentials file." required:"true"`
+}
+
+func main() {
+	args := Args{}
+
+	_, err := flags.Parse(&args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	opts := option.WithCredentialsFile(args.CredentialsFile)
+
+	client, err := dlp.NewClient(ctx, opts)
+	if err != nil {
+		log.Fatalf("error creating DLP client: %v", err)
+	}
+
+	res, err := client.ListInfoTypes(ctx, &dlppb.ListInfoTypesRequest{})
+	if err != nil {
+		log.Fatalf("error retrieving InfoTypes: %v", err)
+	}
+
+	fmtutil.PrintJSON(res)
+
+	for i, t := range res.InfoTypes {
+		fmt.Printf("%v) %v\n", i+1, t.Name)
+	}
+
+	fmt.Println("DONE")
+}
--- dlp/v2/examples/hello_world/main.go
+++ dlp/v2/examples/hello_world/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/grokify/gotilla/fmt/fmtutil"
+	"github.com/jessevdk/go-flags"
+	"google.golang.org/api/option"
 
+	dlp "cloud.google.com/go/dlp/apiv2"
 	dlpu "github.com/grokify/googleutil/dlp/v2"
+	gu "github.com/grokify/oauth2more/google"
 	dlppb "google.golang.org/genproto/googleapis/privacy/dlp/v2"
 )
 
@@ -17,9 +23,34 @@ https://github.com/GoogleCloudPlatform/golang-samples/blob/master/dlp/dlp_quicks
 
 */
 
+type Args struct {
+	// Service Account Credentials File
+	CredentialsFile string `short:"c" long:"credentials" description:"Path to crdentials file." required:"true"`
+}
+
 func main() {
-	projectID := "PROJECT_ID"
-	input := "Hello World"
+	args := Args{}
+
+	_, err := flags.Parse(&args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	opts := option.WithCredentialsFile(args.CredentialsFile)
+
+	client, err := dlp.NewClient(ctx, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	creds, err := gu.NewCredentialsFromFile(args.CredentialsFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	projectID := creds.ProjectID
+	input := "Hello World 680-26-5240"
 
 	inspectConfig := &dlppb.InspectConfig{
 		InfoTypes: []*dlppb.InfoType{
@@ -41,6 +72,24 @@ func main() {
 		Item:          dlpu.NewContentDataItemSimple(input)}
 
 	fmtutil.PrintJSON(req)
+
+	// Run request.
+	resp, err := client.InspectContent(ctx, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	findings := resp.GetResult().GetFindings()
+	if len(findings) == 0 {
+		fmt.Println("No findings.")
+	}
+	fmt.Println("Findings:")
+	for _, f := range findings {
+		if inspectConfig.IncludeQuote {
+			fmt.Println("\tQuote: ", f.GetQuote())
+		}
+		fmt.Println("\tInfo type: ", f.GetInfoType().GetName())
+		fmt.Println("\tLikelihood: ", f.GetLikelihood())
+	}
 
 	fmt.Println("DONE")
 }
