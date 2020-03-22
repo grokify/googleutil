a58c4d3c68d51e5f3e9978fa24f7cd75a178c127
John Wang <johncwang@gmail.com>
2020-03-21 20:20:28 -0700

    add gmailutil

---
+++ gmailutil/v1/messages_list.go
@@ -0,0 +1,60 @@
+package gmail
+
+import (
+	"strings"
+	"time"
+
+	"github.com/grokify/gotilla/time/timeutil"
+)
+
+const (
+	GmailDateFormat  = "2006/01/02"
+	ReferenceURL     = "https://developers.google.com/gmail/api/v1/reference"
+	TutorialURLGO    = "https://developers.google.com/gmail/api/quickstart/go"
+	ListApiReference = "https://developers.google.com/gmail/api/v1/reference/users/messages/list"
+	ListApiExample   = "https://stackoverflow.com/questions/43057478/google-api-go-client-listing-messages-w-label-and-fetching-header-fields"
+	FilteringExample = "https://developers.google.com/gmail/api/guides/filtering"
+	FilterRules      = "https://support.google.com/mail/answer/7190"
+)
+
+// ?q=in:sent after:1388552400 before:1391230800
+// ?q=in:sent after:2014/01/01 before:2014/02/01 ?q=in:sent after:2014/01/01 before:2014/02/01
+// "from:someuser@example.com rfc822msgid:<somemsgid@example.com> is:unread"
+
+/*
+
+Warning: All dates used in the search query are interpretted as midnight on that date in the PST timezone. To specify accurate dates for other timezones pass the value in seconds instead:
+
+*/
+
+type MessageListQueryOpts struct {
+	In          string
+	From        string
+	RFC822msgid string
+	After       time.Time
+	Before      time.Time
+	Interval    timeutil.Interval
+}
+
+func GenerateMessageListQueryString(opts MessageListQueryOpts) string {
+	parts := []string{}
+	opts.From = strings.TrimSpace(opts.From)
+	if len(opts.From) > 0 {
+		parts = append(parts, "from:"+opts.From)
+	}
+	opts.In = strings.TrimSpace(opts.In)
+	if len(opts.In) > 0 {
+		parts = append(parts, "in:"+opts.In)
+	}
+	opts.RFC822msgid = strings.TrimSpace(opts.RFC822msgid)
+	if len(opts.RFC822msgid) > 0 {
+		parts = append(parts, "rfc822msgid:"+opts.RFC822msgid)
+	}
+	if !timeutil.TimeIsZeroAny(opts.After) {
+		parts = append(parts, "after:"+opts.After.Format(GmailDateFormat))
+	}
+	if !timeutil.TimeIsZeroAny(opts.Before) {
+		parts = append(parts, "before:"+opts.Before.Format(GmailDateFormat))
+	}
+	return strings.Join(parts, " ")
+}
---
+++ gmailutil/v1/messages_list_test.go
@@ -0,0 +1,47 @@
+package gmail
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+var listQueryStringTests = []struct {
+	qString  string
+	qAfter   string
+	qBefore  string
+	qOptions MessageListQueryOpts
+}{
+	{"from:foo@example.com", "", "", MessageListQueryOpts{From: "foo@example.com"}},
+	{"in:Inbox", "", "", MessageListQueryOpts{In: "Inbox"}},
+	{"after:2016/01/02 before:2019/11/12", "2016-01-02T00:00:00Z", "2019-11-12T00:00:00Z", MessageListQueryOpts{}},
+}
+
+// TestGenerateMessageListQueryString creates a gmail query string.
+func TestGenerateMessageListQueryString(t *testing.T) {
+	for _, tt := range listQueryStringTests {
+		qOptions := tt.qOptions
+		jsonOpts, err := json.Marshal(qOptions)
+		if len(tt.qAfter) > 0 {
+			dt, err := time.Parse(time.RFC3339, tt.qAfter)
+			if err != nil {
+				t.Errorf("gmailutil.GenerateMessageListQueryString('%s') Error: [%v]", jsonOpts, err.Error())
+			}
+			qOptions.After = dt
+		}
+		if len(tt.qBefore) > 0 {
+			dt, err := time.Parse(time.RFC3339, tt.qBefore)
+			if err != nil {
+				t.Errorf("gmailutil.GenerateMessageListQueryString('%s') Error: [%v]", jsonOpts, err.Error())
+			}
+			qOptions.Before = dt
+		}
+		gotString := GenerateMessageListQueryString(qOptions)
+		if gotString != tt.qString {
+			if err != nil {
+				t.Errorf("gmailutil.GenerateMessageListQueryString() Error: [%v]", err.Error())
+			}
+			t.Errorf("gmailutil.GenerateMessageListQueryString('%s') Error: want [%v] got [%v]", jsonOpts, tt.qString, gotString)
+		}
+	}
+}
