c993276177d21c329276d2b636289d2c3fc33624
John Wang <johncwang@gmail.com>
2020-04-13 02:27:58 -0700

    add Google Slides image code

--- slidesutil/v1/image_create_slide.go
+++ slidesutil/v1/image_create_slide.go
@@ -1,47 +1,53 @@
 package slidesutil
 
 import (
-	"github.com/grokify/gotilla/fmt/fmtutil"
-	log "github.com/sirupsen/logrus"
+	"github.com/pkg/errors"
 	"google.golang.org/api/slides/v1"
 )
 
-// CreateSlideImage creates a slide using a main image
+// CreateSlideImageRequestsSidebarRight creates a slide using a main image
 // as the body given a PresentationID, title, and imageURL.
-func CreateSlideImage(srv *slides.Service, psv *slides.PresentationsService, presentationID, titleText, bodyMarkdown string, underlineLinks bool) error {
-	reqs1 := []*slides.Request{CreateSlideRequestLayout(LayoutTitleOnly)}
-
-	resp1, err := psv.BatchUpdate(
-		presentationID,
-		&slides.BatchUpdatePresentationRequest{Requests: reqs1}).Do()
-	if err != nil {
-		return err
+func CreateSlideImageRequestsSidebarRight(slideID, imageID, imageURL, sidebarText string) ([]*slides.Request, error) {
+	emu4M := slides.Dimension{Magnitude: 6000000, Unit: "EMU"}
+	requests := []*slides.Request{
+		{
+			CreateImage: &slides.CreateImageRequest{
+				ObjectId: imageID,
+				Url:      imageURL,
+				ElementProperties: &slides.PageElementProperties{
+					PageObjectId: slideID,
+					Size: &slides.Size{
+						Height: &emu4M,
+						Width:  &emu4M},
+					Transform: &slides.AffineTransform{
+						ScaleX:     1.15,
+						ScaleY:     1.15,
+						TranslateX: 400000.0,
+						TranslateY: -300000.0,
+						Unit:       "EMU"},
+				},
+			},
+		},
 	}
-
-	if 1 == 0 {
-		slideID := resp1.Replies[0].CreateSlide.ObjectId
-		log.Infof("CREATED SLIDE [%v]\n", slideID)
+	if len(sidebarText) > 0 {
+		textboxes := CreateShapeTextBoxRequestInfo{
+			PageId:        slideID,
+			ObjectId:      imageID + ":txt",
+			Text:          sidebarText,
+			Width:         90,
+			Height:        50,
+			FontSize:      10,
+			FontSizeUnit:  UnitPT,
+			DimensionUnit: UnitPT,
+			LocationX:     580,
+			LocationY:     158,
+			LocationUnit:  UnitPT,
+		}
+		tbreq, err := textboxes.Requests()
+		if err != nil {
+			return requests, errors.Wrap(err, "")
+		}
+		requests = append(requests, tbreq...)
 	}
-	//log.Info(`== Fetch "main point" slide title (textbox) ID`)
-	presentation, err := srv.Presentations.Get(presentationID).Do()
-	fmtutil.PrintJSON(presentation)
-	if err != nil {
-		return err
-	}
-	newSlide := presentation.Slides[len(presentation.Slides)-1]
-	fmtutil.PrintJSON(presentation.Slides)
-
-	newSlideTitleID := newSlide.PageElements[0].ObjectId
-	//newSlideBodyTextboxID := newSlide.PageElements[1].ObjectId
-
-	cm := NewCommonMarkData(bodyMarkdown)
-	cm.Inflate()
-	//fmtutil.PrintJSON(cm.Lines())
-
-	reqs2 := []*slides.Request{InsertTextRequest(newSlideTitleID, titleText)}
-
-	_, err = psv.BatchUpdate(
-		presentationID,
-		&slides.BatchUpdatePresentationRequest{Requests: reqs2}).Do()
-	return err
+	return requests, nil
 }
