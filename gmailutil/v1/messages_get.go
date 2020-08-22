931dce9c8ca9455f64096aa8447b1ab95ea4248c
John Wang <johncwang@gmail.com>
2020-08-22 08:28:02 -0700

    feat: gmail: add messages.get

---
+++ gmailutil/v1/messages_get.go
@@ -0,0 +1,16 @@
+package gmailutil
+
+import (
+	"strings"
+
+	"google.golang.org/api/gmail/v1"
+)
+
+func GetMessage(gs *GmailService, userId, messageId string) (*gmail.Message, error) {
+	userId = strings.TrimSpace(userId)
+	messageId = strings.TrimSpace(messageId)
+
+	userMessagesListCall := gs.UsersService.Messages.Get(
+		userId, messageId)
+	return userMessagesListCall.Do()
+}
