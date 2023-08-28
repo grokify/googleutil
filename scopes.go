1a420b37d37403397d400aec30aecccde75a01f3
John Wang <johncwang@gmail.com>
2023-08-28 00:27:40 -0700

    feat: scopes: init

---
+++ scopes.go
@@ -0,0 +1,13 @@
+package googleutil
+
+import (
+	sheets "google.golang.org/api/sheets/v4"
+)
+
+const (
+	ScopeDrive          = sheets.DriveScope
+	ScopeDriveFile      = sheets.DriveFileScope
+	ScopeDriveReadonly  = sheets.DriveReadonlyScope
+	ScopeSlides         = sheets.SpreadsheetsScope
+	ScopeSlidesReadonly = sheets.SpreadsheetsReadonlyScope
+)
