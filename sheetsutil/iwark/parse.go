0787da8d6a64e3650b092ba78c01b317b5bb78a7
John Wang <johncwang@gmail.com>
2024-10-06 15:15:10 -0700

    refactor: `sheetsutil/iwark`: update function names

--- sheetsutil/iwark/parse.go
+++ sheetsutil/iwark/parse.go
@@ -13,7 +13,7 @@ import (
 
 var ErrSheetIDRequired = errors.New("sheet id is required")
 
-func ParseSheetIDTable(client *http.Client, sheetID string, sheetIdx, headerRows uint) (*table.Table, error) {
+func ParseTableFromSheetID(client *http.Client, sheetID string, sheetIdx, headerRows uint) (*table.Table, error) {
 	if strings.Contains(sheetID, "/") {
 		id, _, err := docsutil.ParseDocsURL(sheetID, docsutil.DocSlugSpreadsheet)
 		if err == nil && id != "" {
@@ -28,19 +28,27 @@ func ParseSheetIDTable(client *http.Client, sheetID string, sheetIdx, headerRows
 	if ss, err := service.FetchSpreadsheet(sheetID); err != nil {
 		return nil, err
 	} else {
-		return ParseSpreadsheetTable(ss, sheetIdx, headerRows)
+		return ParseTableFromSpreadsheet(ss, sheetIdx, headerRows)
 	}
 }
 
-func ParseSpreadsheetTable(ss spreadsheet.Spreadsheet, sheetIdx, headerRows uint) (*table.Table, error) {
+func ParseTableFromSpreadsheet(ss spreadsheet.Spreadsheet, sheetIdx, headerRows uint) (*table.Table, error) {
 	if s, err := ss.SheetByIndex(sheetIdx); err != nil {
 		return nil, err
 	} else {
-		return ParseSheetTable(s, headerRows), nil
+		return PraseTableFromSheet(s, headerRows), nil
 	}
 }
 
-func ParseSheet(s *spreadsheet.Sheet, headerRows uint) ([]string, [][]string) {
+func PraseTableFromSheet(s *spreadsheet.Sheet, headerRows uint) *table.Table {
+	cols, rows := ParseDataFromSheet(s, headerRows)
+	tbl := table.NewTable("")
+	tbl.Columns = cols
+	tbl.Rows = rows
+	return &tbl
+}
+
+func ParseDataFromSheet(s *spreadsheet.Sheet, headerRows uint) ([]string, [][]string) {
 	var cols []string
 	var rows [][]string
 	for i, srow := range s.Rows {
@@ -58,12 +66,4 @@ func ParseSheet(s *spreadsheet.Sheet, headerRows uint) ([]string, [][]string) {
 	}
 	cols = stringsutil.SliceTrimSpace(cols, false)
 	return cols, rows
-}
-
-func ParseSheetTable(s *spreadsheet.Sheet, headerRows uint) *table.Table {
-	cols, rows := ParseSheet(s, headerRows)
-	tbl := table.NewTable("")
-	tbl.Columns = cols
-	tbl.Rows = rows
-	return &tbl
 }
